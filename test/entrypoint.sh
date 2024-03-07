668e687c6fb60275fa1dfdd78071cf1b9c0ac26e
kshitijson <kshitijson4@gmail.com>
2024-03-08 00:35:50 +0530

    Attached a volume to the couchdb container to persist the _users and _replicator, still getting connection issues in couchdbPD101

--- test/entrypoint.sh
+++ test/entrypoint.sh
@@ -6,10 +6,10 @@ sleep 5
 
 # Execute other setup commands (e.g., create _users database)
 echo "Creating System Database user"
-./wait-for-it.sh localhost:5984 -- curl -X PUT -u admin:adminpw http://localhost:5984/_users
+./wait-for-it.sh localhost:6984 -- curl -X PUT -u admin:adminpw http://localhost:6984/_users
 
 sleep 2
 
 echo "Creating System Database replicator"
-./wait-for-it.sh localhost:5984 -- curl -X PUT -u admin:adminpw http://localhost:5984/_replicator
+./wait-for-it.sh localhost:6984 -- curl -X PUT -u admin:adminpw http://localhost:6984/_replicator
 

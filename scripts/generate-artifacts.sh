505c74456bb85f85d75c50c8be89e2e8fe791ebe
kshitijson <kshitijson4@gmail.com>
2024-03-16 00:16:11 +0530

    Edited configtx.yaml as per the latest norms, had to mention only one profile which was the genesis

---
+++ scripts/generate-artifacts.sh
@@ -0,0 +1,42 @@
+# echo "######   Deleting crypto-config     #######"
+# rm -rf ./crypto-config
+
+# echo ""
+# echo "######   Generating crypto-config     #######"
+# cryptogen generate --config=./crypto-config.yaml --output=./crypto-config
+
+echo ""
+echo "##### Deleting existing files #####"
+rm ../artifacts/mainchannel.block
+# rm evidencevault-genesis.block
+# rm mainchannel.tx
+# rm Policeanchors.tx
+# rm Forensicanchors.tx
+
+
+# SYS_CHANNEL="ordererchannel"
+# CHANNEL_NAME="mainchannel"
+
+# echo ""
+# echo "##### Generating Genesis Block #####"
+# configtxgen -profile EvidenceVaultOrdererGenesis -channelID $SYS_CHANNEL -outputBlock evidencevault-genesis.block
+
+# echo ""
+# echo "##### Generating Channel Configuration Transaction #####"
+# configtxgen -profile EvidenceVaultChannel -outputCreateChannelTx mainchannel.tx -channelID $CHANNEL_NAME
+
+# echo ""
+# echo "##### Generating anchor peer update for PoliceDepartment #####"
+# configtxgen -profile EvidenceVaultChannel -outputAnchorPeersUpdate Policeanchors.tx -channelID $CHANNEL_NAME -asOrg Police
+
+# echo ""
+# echo "##### Generating anchor peer update for ForensicDepartment #####"
+# configtxgen -profile EvidenceVaultChannel -outputAnchorPeersUpdate Forensicanchors.tx -channelID $CHANNEL_NAME -asOrg Forensic
+
+
+
+# channel name defaults to "mychannel"
+CHANNEL_NAME="mainchannel"
+
+# Generate Channel Genesis block
+configtxgen -profile EvidenceVaultOrdererGenesis -configPath ../artifacts/ -channelID $CHANNEL_NAME  -outputBlock $CHANNEL_NAME.block

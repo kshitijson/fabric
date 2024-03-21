dc63741c7ae2893b835628d52a79af05fbf2c7e1
kshitijson <kshitijson4@gmail.com>
2024-03-22 00:14:42 +0530

    Successffully joined PD100 peer, but facing error in FD100 as they are anchor peer, just make both of them join the channel

--- scripts/channel.sh
+++ scripts/channel.sh
@@ -1,7 +1,7 @@
 export CORE_PEER_TLS_ENABLED=true
 export ORDERER_CA=${PWD}/../artifacts/crypto-config/ordererOrganizations/evidence-vault.com/orderers/orderer.evidence-vault.com/msp/tlscacerts/tlsca.evidence-vault.com-cert.pem
-export PEER0_ORG1_CA=${PWD}/../artifacts/channel/crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
-export PEER0_ORG2_CA=${PWD}/../artifacts/channel/crypto-config/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+# export PEER0_ORG1_CA=${PWD}/../artifacts/channel/crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+# export PEER0_ORG2_CA=${PWD}/../artifacts/channel/crypto-config/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
 export FABRIC_CFG_PATH=${PWD}/../artifacts/config/
 
 
@@ -11,7 +11,7 @@ export ORDERER_ADMIN_TLS_PRIVATE_KEY=${PWD}/../artifacts/crypto-config/ordererOr
 
 CHANNEL_NAME='mainchannel'
 
-channelJoinOrderer(){
+channelJoinOrderer() {
     osnadmin channel join --channelID $CHANNEL_NAME \
     --config-block ../channel-artifacts/${CHANNEL_NAME}.block -o localhost:7053 \
     --ca-file $ORDERER_CA \
@@ -19,4 +19,17 @@ channelJoinOrderer(){
     --client-key $ORDERER_ADMIN_TLS_PRIVATE_KEY 
 }
 
-channelJoinOrderer
+
+peerJoinChannel() {
+    # for PD100
+    export CORE_PEER_LOCALMSPID="PoliceMSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/../artifacts/crypto-config/peerOrganizations/police.evidence-vault.com/peers/PD100.police.evidence-vault.com/tls/ca.crt
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/../artifacts/crypto-config/peerOrganizations/police.evidence-vault.com/users/Admin@police.evidence-vault.com/msp
+    export CORE_PEER_ADDRESS=localhost:7051
+
+    peer channel join -b ../channel-artifacts/${CHANNEL_NAME}.block
+    ##
+}
+
+# channelJoinOrderer
+peerJoinChannel
